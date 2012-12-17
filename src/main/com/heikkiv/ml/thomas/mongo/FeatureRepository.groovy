7fcc2d693d1d5b97d73b8e5606860c443eaa8750
Heikki Verta <heikki.verta@gmail.com>
2012-12-17 23:00:05 +0200

    Removed old repositories

--- src/main/com/heikkiv/ml/thomas/mongo/FeatureRepository.groovy
+++
@@ -1,9 +0,0 @@
-package com.heikkiv.ml.thomas.mongo
-
-public interface FeatureRepository {
-
-    void incrementFeatureCount(String feature, String category)
-
-    int getFeatureCountInCategory(String feature, String category)
-
-}
